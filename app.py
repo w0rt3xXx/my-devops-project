9ec43ed3190bb1e476e93687b3580ed6aa8345f6
Komissarov Kirill <komissarov.kirill@lll.kpi.ua>
2025-10-01 18:58:03 +0300

    Initial commit: Add simple Flask application

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # Это сообщение, которое будет отображаться в браузере
+    return "Привет от DevOps проекта! Я работаю!"
+
+if __name__ == "__main__":
+    # Приложение будет работать на порту 5000
+    port = int(os.environ.get("PORT", 5000))
+    # host='0.0.0.0' делает приложение доступным извне контейнера, что нам понадобится позже
+    app.run(debug=True, host='0.0.0.0', port=port)
